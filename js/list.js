ee887cdf7e1e2b1947778aa3ad5e1ea67633bfb0
Dainius <dainiusnarkevicius1@gmail.com>
2023-06-20 16:17:50 +0300

    update

---
+++ js/list.js
@@ -0,0 +1,19 @@
+const goods = [
+    { name: 'Morka', price: 1.1, },
+    { name: 'Agurkas', price: 3.7, },
+    { name: 'Pomidoras', price: 4.3, },
+    { name: 'Bulve', price: 2, },
+    { name: 'Kalafijoras', price: 1.56, },
+    { name: 'Petrazole', price: 1.23, },
+    { name: 'Citrina', price: 1, },
+];
+
+const listDOM = document.querySelector('ul');
+
+
+for (const { name, price } of goods){
+    const formatedPrice = price.toFixed(2)
+    listDOM.innerHTML += `<li>${name} (${formatedPrice} Eur)</li>`;
+
+}
+
